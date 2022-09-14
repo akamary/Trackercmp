8e2b7cf1d4f23eea1cb6270061adaf15cae85c63
Aviv Kamary <kamaryaviv@gmail.com>
2022-09-14 06:13:12 +0300

    adding PUT req

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,9 +2,11 @@ package com.example.demo.student;
 
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 // Service layer
@@ -38,5 +40,26 @@ public class StudentService {
             );
         }
         studentRepository.deleteById(studentId);
+    }
+
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId). orElseThrow(()->new IllegalStateException(
+                "student with id " + studentId + " does not exists"
+        ));
+
+        if(name != null && name.length()>0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if(email != null && email.length()>0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+        }
+        student.setEmail(email);
+
     }
 }
